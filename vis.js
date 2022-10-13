f28463b0c1e4afe73eeb43676618cff9195b2293
mkam0020 <mkam0020@student.monash.edu>
2022-10-13 23:16:50 +1100

    improved

---
+++ vis.js
@@ -0,0 +1,14 @@
+var vg_1 = "Main.vg.json";
+vegaEmbed("#Main", vg_1).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "PieChart.vg.json";
+vegaEmbed("#PieChart", vg_2).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 = "LogHistogram.vg.json";
+vegaEmbed("#LogHistogram", vg_3).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
